66fb4d951eef2137f79c148a3ad0a92397de96fd
AllaPavlovska <pavlovskaa87@gmail.com>
2025-03-05 06:40:17 -0500

    first commit

---
+++ JosephusProblem/Program.cs
@@ -0,0 +1,69 @@
+﻿
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.IO;
+// using Newtonsoft.Json;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         // Створення словника
+//         Dictionary<int, string> dictionary = new Dictionary<int, string>()
+//         {
+//             {1, "One"},
+//             {2, "Two"},
+//             {3, "Three"},
+//             {4, "Four"},
+//             {5, "Five"},
+//             {6, "Six"}
+//         };
+
+//         // Запит користувача на введення порогового значення для ключа
+//         Console.Write("Введіть порогове значення ключа: ");
+//         int threshold = int.Parse(Console.ReadLine());
+
+//         // Фільтруємо словник, де ключі більші або дорівнюють заданому
+//         var filteredDictionary = dictionary.Where(kv => kv.Key >= threshold)
+//                                            .ToDictionary(kv => kv.Key, kv => kv.Value);
+
+//         // Виведення результату
+//         Console.WriteLine("Результат після фільтрації:");
+//         foreach (var item in filteredDictionary)
+//         {
+//             Console.WriteLine($"{item.Key}: {item.Value}");
+//         }
+
+//         // Збереження відфільтрованого словника у JSON файл
+//         string json = JsonConvert.SerializeObject(filteredDictionary, Formatting.Indented);
+//         File.WriteAllText("filteredDictionary.json", json);
+
+//         Console.WriteLine("\nРезультат збережено в файл 'filteredDictionary.json'.");
+//     }
+// }
+
+
+// 3завд
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Дано послідовність цілих чисел
+        int[] numbers = { -5, 3, -2, 8, 0, -1, 7, 4, -9 };
+
+        // Використовуємо LINQ для фільтрації додатних чисел
+        var positiveNumbers = numbers.Where(n => n > 0).ToArray();
+
+        // Виведення результату
+        Console.WriteLine("Додатні числа:");
+        foreach (var num in positiveNumbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ Laba1/Program.cs
@@ -0,0 +1,65 @@
+﻿
+// завд1
+//  ArrayList
+
+using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть кількість людей N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        ArrayList people = new ArrayList();
+        for (int i = 1; i <= n; i++)
+        {
+            people.Add(i);
+        }
+
+        int index = 0;
+        while (people.Count > 1)
+        {
+            index = (index + 1) % people.Count; // Переходимо до наступного
+            people.RemoveAt(index); // Видаляємо кожного другого
+
+            // Оскільки після видалення індекси зміщуються, коригуємо індекс
+            if (index == people.Count)
+                index = 0;
+        }
+
+        Console.WriteLine("Остання людина: " + people[0]);
+    }
+}
+
+// LinkeLIst
+
+// using System;
+// using System.Collections.Generic;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.Write("Введіть кількість людей N: ");
+//         int n = int.Parse(Console.ReadLine());
+
+//         LinkedList<int> people = new LinkedList<int>();
+//         for (int i = 1; i <= n; i++)
+//         {
+//             people.AddLast(i);
+//         }
+
+//         LinkedListNode<int> current = people.First;
+//         while (people.Count > 1)
+//         {
+//             LinkedListNode<int> toRemove = current.Next ?? people.First; // Наступний або перший
+//             current = toRemove.Next ?? people.First; // Переходимо через одного
+//             people.Remove(toRemove);
+//         }
+
+//         Console.WriteLine("Остання людина: " + people.First.Value);
+//     }
+// }
+
